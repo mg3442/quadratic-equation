79a8e1b6cf7adfc1d1e748797fcf8157b48c6eaa
mg3442 <magnusprotas@gmail.com>
2018-09-23 21:50:12 +0300

    complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+  
+  var y,discr,x1,x2,arr=[],a,b,c;
+  
+	y = equation.split(" ");
+	
+	 a = y[0]; 
+	 b = y[3]+y[4];
+	 c = y[7]+y[8];
+	discr = (b*b) - (4*a*c);
+	
+	x1 = ((-b) + (Math.sqrt(discr)))/(2*a);
+	x2 = ((-b) - (Math.sqrt(discr)))/(2*a);
+	var roundX1 = Math.round(x1);
+	var roundX2 = Math.round(x2);
+	
+	arr.push(roundX1);
+	arr.push(roundX2);
+	
+	arr.sort(function(a,b) {return a-b});
+	return arr
+
 }
